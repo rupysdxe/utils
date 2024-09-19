1920073c216298eaacc80452e9e0a9e79e964660
Rupesh Dangi <83266954+rupysdxe@users.noreply.github.com>
2024-09-19 16:08:10 +0545

    feat: add number utils.

---
+++ java/NumberUtils.java
@@ -0,0 +1,15 @@
+/**
+ * @author Rupesh Dangi
+ * @date: 2024/09/19 14/50
+ */
+public final class NumberUtils {
+    public static final long BILLION = 1_000_000_000;
+    public static final long MILLION = 1_000_000;
+    public static final long THOUSAND = 1_000L;
+    public static final long HUNDRED = 100L;
+    private static final long TEN = 10L;
+    private static final long ONE = 1L;
+
+    private NumberUtils() {}
+
+}
